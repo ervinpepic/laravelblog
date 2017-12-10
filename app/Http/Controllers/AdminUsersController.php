9ee90653c52aa05bc562d07dde7d62d7e0b576b7
Ervin Pepic <ervin.hack@gmail.com>
2017-12-10 19:34:08 +0100

    Added security layer

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -6,6 +6,7 @@ use App\Photo;
 use App\Role;
 use App\User;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UserEditRequest;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -49,7 +50,18 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
 
-        $input = $request->all();
+        if (trim($request->password) == '') {
+
+            $input = $request->except('password');
+
+        } else {
+
+            $input = $request -> all();
+            $input['password'] = bcrypt($request -> password);
+
+        }
+
+
 
         if ($file = $request->file('photo_id')) {
 
@@ -63,9 +75,6 @@ class AdminUsersController extends Controller
         $input['password'] = bcrypt($request->password);
 
         User::create($input);
-
-
-        User::create($request->all());
 
         return redirect('/admin/users');
 
@@ -95,7 +104,10 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
 
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.edit', compact('user', 'roles'));
 
     }
 
@@ -107,9 +119,37 @@ class AdminUsersController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function update(Request $request, $id)
+    public function update(UserEditRequest $request, $id)
     {
-        //
+
+        $user = User::findOrFail($id);
+
+        if (trim($request->password) == '') {
+
+            $input = $request->except('password');
+
+        } else {
+
+
+            $input['password'] = bcrypt($request -> password);
+
+        }
+
+        if ($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
+
     }
 
     /**
--- app/Photo.php
+++ app/Photo.php
@@ -7,8 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
 
+    protected $uploaded_images = '/images/';
+
     protected $fillable = ['file'];
 
+    public function getFileAttribute($photo) {
 
+        return $this->uploaded_images . $photo;
+
+    }
 
 }
