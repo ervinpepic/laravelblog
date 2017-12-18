04521da38182782b514597c9141b30b41f6cd2ab
Ervin Pepic <ervin.hack@gmail.com>
2017-12-18 19:35:14 +0100

    Upgrade to Laravel 5.3.6

--- app/User.php
+++ app/User.php
@@ -64,4 +64,12 @@ class User extends Authenticatable
 
     }
 
+    public function getGravatarAttribute() {
+
+        $hash = md5(strtolower(trim($this->attributes['email']))) . "?d=mm&s=";
+
+        return "http://www.gravatar.com/avatar/$hash";
+
+    }
+
 }
